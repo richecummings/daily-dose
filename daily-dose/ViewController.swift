480b40e7a7dee7c508d880a9c61972f0e0ee2fd7
Richard Cummings <richardecummings@gmail.com>
2018-08-20 18:12:20 -0400

    Initial Commit

---
+++ daily-dose/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  daily-dose
+//
+//  Created by Richard Cummings on 2018-08-20.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
