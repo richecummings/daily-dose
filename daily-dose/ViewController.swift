41edaa7765301afce8cc7fed69ee06fafa0749b9
Richard Cummings <richardecummings@gmail.com>
2019-02-19 20:47:10 -0500

    Implementing Google AdMob

--- daily-dose/ViewController.swift
+++ daily-dose/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bannerView: GADBannerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
     }
 
     override func didReceiveMemoryWarning() {
